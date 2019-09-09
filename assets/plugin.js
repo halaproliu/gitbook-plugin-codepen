d5359f1340b21b775eec72930c980653b330fe30
luffy <luffybryant@gmail.com>
2019-09-09 23:09:14 +0800

    update 0.0.4

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,21 +1,17 @@
-"use strict";
-
-var _gitbook = _interopRequireDefault(require("gitbook"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var loadScript = function loadScript(url) {
-  var script = document.createElement('script');
-  script.src = url;
-  script.async = 1;
-  document.getElementsByTagName('head')[0].appendChild(script);
-};
-
-_gitbook["default"].events.bind('start', function (e, config) {
-  var pluginConfig = config.codepen;
-
-  if (pluginConfig) {
-    var url = 'https://static.codepen.io/assets/embed/ei.js';
-    loadScript(url);
+require(['gitbook'], function(gitbook) {
+  var loadScript = function loadScript(url) {
+    var script = document.createElement('script')
+    script.src = url
+    script.async = 1
+    document.getElementsByTagName('head')[0].appendChild(script)
   }
-});
+
+  gitbook.events.bind('start', function(e, config) {
+    var pluginConfig = config.codepen
+
+    if (pluginConfig) {
+      var url = 'https://static.codepen.io/assets/embed/ei.js'
+      loadScript(url)
+    }
+  })
+})
